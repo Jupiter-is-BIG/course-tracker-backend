2f2c1ae8db821dfbce0c5a6bd5b88f25b35a7dca
Anand <clef@Anands-MacBook-Pro.local>
2023-12-23 04:06:42 -0800

    HOTFIX: Alembic

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI
 from .utils.db import get_db
 from sqlalchemy.orm import Session
 from .routes import script, scrape, request, user
+from .models import admin
 import logging
 
 app = FastAPI()
@@ -16,5 +17,13 @@ app.include_router(user.router)
 
 @app.get("/")
 async def root(db: Session = Depends(get_db)):
+    admin_init = db.query(admin.Admin).first()
+    if not admin_init:
+        new_admin = admin.Admin(
+            search_active = True,
+            frequency = 600,
+        )
+        db.add(new_admin)
+        db.commit()
     return {"message": "Course Tracker Backend v0.0.1"}
 
