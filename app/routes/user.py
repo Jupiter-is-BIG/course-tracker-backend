c990f976424cd3270c2904c137a99945471b1ab6
Anand <clef@Anands-MacBook-Pro.local>
2023-12-23 05:10:07 -0800

    login route

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from ..models import user
 from sqlalchemy.orm import Session
-from sqlalchemy import or_
+from sqlalchemy import or_, and_
 from ..utils.db import get_db
 from ..config import settings
 
@@ -30,6 +30,18 @@ async def create_user(user_id: str, user_name: str, password: str, db: Session =
     db.commit()
 
     return {"message": "User registered successfully"}
+
+@router.get("/login", status_code=200)
+async def login(user_name: str, password: str, db: Session = Depends(get_db)):
+    """ login route for user """
+    auth_search = db.query(user.User).filter(and_(user.User.user_name == user_name, user.User.password == password)).first()
+    if not auth_search:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Wrong password and username combination"
+        )
+    
+    return {"message" : "Login successful"}
 
 
 
